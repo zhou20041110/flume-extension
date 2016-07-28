f5c9ee5915db8a59acd14a7ff9d7feb7be89d4a6
yurun <miao.work2010@gmail.com>
2016-07-28 19:43:49 +0800

    dev

--- src/main/java/org/apache/hadoop/hive/ql/io/orc/ORCFileReaderMain.java
+++ src/main/java/org/apache/hadoop/hive/ql/io/orc/ORCFileReaderMain.java
@@ -39,7 +39,7 @@ public class ORCFileReaderMain {
 
 		NullWritable key = NullWritable.get();
 
-		OrcStruct value = new OrcStruct(3);
+		OrcStruct value = null;
 
 		while (reader.next(key, value)) {
 			System.out.println(value);
