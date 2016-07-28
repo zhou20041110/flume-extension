317d905f27f04faa7cb0060b9f7bd4398780f021
yurun <miao.work2010@gmail.com>
2016-07-28 18:18:45 +0800

    dev

--- src/main/java/org/apache/hadoop/hive/ql/io/orc/ORCFileWriterMain.java
+++ src/main/java/org/apache/hadoop/hive/ql/io/orc/ORCFileWriterMain.java
@@ -12,6 +12,7 @@ import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.typeinfo.StructTypeInfo;
 import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
 import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoUtils;
+import org.apache.hadoop.io.Text;
 
 /**
  * @author yurun
@@ -46,21 +47,21 @@ public class ORCFileWriterMain {
 
 		OrcStruct struct = new OrcStruct(columns);
 
-		struct.setFieldValue(0, "1");
-		struct.setFieldValue(1, "2");
-		struct.setFieldValue(2, "3");
+		struct.setFieldValue(0, new Text("1"));
+		struct.setFieldValue(1, new Text("2"));
+		struct.setFieldValue(2, new Text("3"));
 
 		OrcStruct struct2 = new OrcStruct(columns);
 
-		struct2.setFieldValue(0, "4");
-		struct2.setFieldValue(1, "5");
-		struct2.setFieldValue(2, "6");
+		struct2.setFieldValue(0, new Text("4"));
+		struct2.setFieldValue(1, new Text("5"));
+		struct2.setFieldValue(2, new Text("6"));
 
 		OrcStruct struct3 = new OrcStruct(columns);
 
-		struct3.setFieldValue(0, "7");
-		struct3.setFieldValue(1, "8");
-		struct3.setFieldValue(2, "9");
+		struct3.setFieldValue(0, new Text("7"));
+		struct3.setFieldValue(1, new Text("8"));
+		struct3.setFieldValue(2, new Text("9"));
 
 		writer.addRow(struct);
 		writer.addRow(struct2);
