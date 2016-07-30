ef1b35e9105d0c025eee74fae8de24d186c26d18
yurun <miao.work2010@gmail.com>
2016-07-30 08:21:26 +0800

    dev

--- src/main/java/org/apache/hadoop/hive/ql/io/orc/extension/output/OrcMROutputFormatDriver.java
+++ src/main/java/org/apache/hadoop/hive/ql/io/orc/extension/output/OrcMROutputFormatDriver.java
@@ -7,15 +7,18 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.hive.ql.io.orc.OrcMRWritable;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.ql.io.orc.OrcMROutputFormat;
+import org.apache.hadoop.hive.ql.io.orc.OrcMRWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -64,6 +67,9 @@ public class OrcMROutputFormatDriver extends Configured implements Tool {
 		Job job = new Job(conf);
 
 		job.setJarByClass(OrcMROutputFormatDriver.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(OrcMROutputFormat.class);
