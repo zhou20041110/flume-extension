f03ed8a48684c310b9e572a2b12e53a77b202a5f
yurun <miao.work2010@gmail.com>
2016-07-30 08:15:53 +0800

    dev

---
+++ src/main/java/org/apache/hadoop/hive/ql/io/orc/OrcMRInputFormat.java
@@ -0,0 +1,170 @@
+/**
+ * 
+ */
+package org.apache.hadoop.hive.ql.io.orc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+/**
+ * @author yurun
+ *
+ */
+public class OrcMRInputFormat extends InputFormat<NullWritable, OrcMRWritable> {
+
+	private OrcInputFormat orcInputFormat = new OrcInputFormat();
+
+	public static class OrcMRInputSplit extends InputSplit {
+
+		private org.apache.hadoop.mapred.InputSplit split;
+
+		public org.apache.hadoop.mapred.InputSplit getSplit() {
+			return split;
+		}
+
+		public void setSplit(org.apache.hadoop.mapred.InputSplit split) {
+			this.split = split;
+		}
+
+		@Override
+		public long getLength() throws IOException, InterruptedException {
+			return split.getLength();
+		}
+
+		@Override
+		public String[] getLocations() throws IOException, InterruptedException {
+			return split.getLocations();
+		}
+
+	}
+
+	@Override
+	public List<InputSplit> getSplits(JobContext context) throws IOException, InterruptedException {
+		org.apache.hadoop.mapred.InputSplit[] splits = orcInputFormat.getSplits(new JobConf(context.getConfiguration()),
+				0);
+
+		List<InputSplit> inputSplits = new ArrayList<>();
+
+		for (org.apache.hadoop.mapred.InputSplit inputSplit : splits) {
+			OrcMRInputSplit orcMRInputSplit = new OrcMRInputSplit();
+
+			orcMRInputSplit.setSplit(inputSplit);
+
+			inputSplits.add(orcMRInputSplit);
+		}
+
+		return inputSplits;
+	}
+
+	@Override
+	public RecordReader<NullWritable, OrcMRWritable> createRecordReader(InputSplit split, TaskAttemptContext context)
+			throws IOException, InterruptedException {
+		OrcMRInputSplit orcMRInputSplit = (OrcMRInputSplit) split;
+
+		org.apache.hadoop.mapred.InputSplit inputSplit = orcMRInputSplit.getSplit();
+
+		return new RecordReader<NullWritable, OrcMRWritable>() {
+
+			private org.apache.hadoop.mapred.RecordReader<NullWritable, OrcStruct> reader = orcInputFormat
+					.getRecordReader(inputSplit, new JobConf(context.getConfiguration()), new Reporter() {
+
+						@Override
+						public void progress() {
+
+						}
+
+						@Override
+						public void setStatus(String status) {
+
+						}
+
+						@Override
+						public void incrCounter(String group, String counter, long amount) {
+
+						}
+
+						@Override
+						public void incrCounter(Enum<?> key, long amount) {
+
+						}
+
+						@Override
+						public org.apache.hadoop.mapred.InputSplit getInputSplit()
+								throws UnsupportedOperationException {
+							return null;
+						}
+
+						@SuppressWarnings("deprecation")
+						@Override
+						public org.apache.hadoop.mapred.Counters.Counter getCounter(String group, String name) {
+							return null;
+						}
+
+						@SuppressWarnings("deprecation")
+						@Override
+						public org.apache.hadoop.mapred.Counters.Counter getCounter(Enum<?> name) {
+							return null;
+						}
+
+					});
+
+			private NullWritable nullWritable = NullWritable.get();
+
+			private OrcStruct orcStruct = new OrcStruct(0);
+
+			private OrcMRWritable orcMRWritable = new OrcMRWritable();
+
+			@Override
+			public void initialize(InputSplit split, TaskAttemptContext context)
+					throws IOException, InterruptedException {
+
+			}
+
+			@Override
+			public boolean nextKeyValue() throws IOException, InterruptedException {
+				return reader.next(nullWritable, orcStruct);
+			}
+
+			@Override
+			public NullWritable getCurrentKey() throws IOException, InterruptedException {
+				return nullWritable;
+			}
+
+			@Override
+			public OrcMRWritable getCurrentValue() throws IOException, InterruptedException {
+				int columns = orcStruct.getNumFields();
+
+				orcMRWritable.clear();
+
+				for (int index = 0; index < columns; index++) {
+					orcMRWritable.add((Writable) orcStruct.getFieldValue(index));
+				}
+
+				return orcMRWritable;
+			}
+
+			@Override
+			public float getProgress() throws IOException, InterruptedException {
+				return reader.getProgress();
+			}
+
+			@Override
+			public void close() throws IOException {
+				reader.close();
+			}
+
+		};
+	}
+
+}
---
+++ src/main/java/org/apache/hadoop/hive/ql/io/orc/extension/input/OrcMRInputFormatDriver.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package org.apache.hadoop.hive.ql.io.orc.extension.input;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.hive.ql.io.orc.OrcMRInputFormat;
+import org.apache.hadoop.hive.ql.io.orc.OrcMRWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+/**
+ * 
+ * @author yurun
+ *
+ */
+public class OrcMRInputFormatDriver extends Configured implements Tool {
+
+	public static class OrcMRInputFormatMapper extends Mapper<NullWritable, OrcMRWritable, Text, NullWritable> {
+
+		@Override
+		protected void map(NullWritable key, OrcMRWritable value,
+				Mapper<NullWritable, OrcMRWritable, Text, NullWritable>.Context context)
+				throws IOException, InterruptedException {
+			List<Writable> writables = value.gets();
+
+			String line = "";
+
+			int columns = writables != null ? writables.size() : 0;
+
+			for (int index = 0; index < columns; index++) {
+				line += writables.get(index).toString();
+
+				if (index < (columns - 1)) {
+					line += "\t";
+				}
+			}
+
+			context.write(new Text(line), NullWritable.get());
+		}
+
+	}
+
+	public static class OrcMRInputFormatReducer extends Reducer<Text, NullWritable, Text, NullWritable> {
+
+		@Override
+		protected void reduce(Text key, Iterable<NullWritable> values,
+				Reducer<Text, NullWritable, Text, NullWritable>.Context context)
+				throws IOException, InterruptedException {
+			context.write(key, NullWritable.get());
+		}
+
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		Configuration conf = getConf();
+
+		Job job = new Job(conf);
+
+		job.setJarByClass(OrcMRInputFormatDriver.class);
+
+		job.setInputFormatClass(OrcMRInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		job.setMapperClass(OrcMRInputFormatMapper.class);
+
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(NullWritable.class);
+
+		job.setReducerClass(OrcMRInputFormatReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(NullWritable.class);
+
+		return job.waitForCompletion(true) ? 0 : -1;
+	}
+
+	public static void main(String[] args) throws Exception {
+		ToolRunner.run(new OrcMRInputFormatDriver(), args);
+	}
+
+}
